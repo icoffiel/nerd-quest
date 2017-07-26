ca7f3dc809b39f70a29b249b656d6f63d75d08f0
Iain Coffield <icoffiel@nerdery.com>
2017-07-25 20:26:44 -0500

    Initial commit

---
+++ src/main/kotlin/com/nerdery/icoffiel/quest/NerdQuestApplication.kt
@@ -0,0 +1,11 @@
+package com.nerdery.icoffiel.quest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NerdQuestApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(NerdQuestApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/nerdery/icoffiel/quest/NerdQuestApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.nerdery.icoffiel.quest
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class NerdQuestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

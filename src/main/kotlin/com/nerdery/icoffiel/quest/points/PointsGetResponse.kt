4c81fe883e8256464f881390fa7811995255f25b
Iain Coffield <icoffiel@nerdery.com>
2017-08-07 14:36:50 -0500

    Fixing Badge response.

--- src/main/kotlin/com/nerdery/icoffiel/quest/points/PointsGetResponse.kt
+++ src/main/kotlin/com/nerdery/icoffiel/quest/points/PointsGetResponse.kt
@@ -5,12 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.nerdery.icoffiel.quest.badge.BadgeResponse
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class PointsGetResponse(
         @JsonProperty("PlayerName") val playerName: String,
         @JsonProperty("AvatarUrl") val avatarUrl: String,
-        @JsonProperty("Badges") val badges: List<String>,
+        @JsonProperty("Badges") val badges: List<BadgeResponse>,
         @JsonProperty("Effects") val effects: List<String>,
         @JsonProperty("Title") val title: String,
         @JsonProperty("Points") val points: Int,
--- src/main/kotlin/com/nerdery/icoffiel/quest/points/PointsPostResponse.kt
+++ src/main/kotlin/com/nerdery/icoffiel/quest/points/PointsPostResponse.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.nerdery.icoffiel.quest.badge.BadgeResponse
 import com.nerdery.icoffiel.quest.item.Item
 
 data class PointsPostResponse(
@@ -11,7 +12,7 @@ data class PointsPostResponse(
         @JsonProperty("Item") val item: Item?,
         @JsonProperty("Points") val points: Int,
         @JsonProperty("Effects") val effects: List<String>,
-        @JsonProperty("Badges") val badges: List<String>) {
+        @JsonProperty("Badges") val badges: List<BadgeResponse>) {
 
     class Deserializer: ResponseDeserializable<PointsPostResponse> {
         override fun deserialize(content: String): PointsPostResponse? = jacksonObjectMapper().readValue<PointsPostResponse>(content)
